e08b9eddddd49de4b74717a84cebfd0f76697e0a
sajid <mohdsajid8010@gmail.com>
2023-07-18 22:36:26 +0530

    commit4

--- src/components/App.js
+++ src/components/App.js
@@ -28,11 +28,12 @@ const App = () => {
           {
             cartItems.map((obj, i) => {
               return (
-                <li key={i} className="child ">
+                <li key={i} className="child">
                   {obj.name} - ${obj.price}<button onClick={()=>removeItem(i)}>Remove</button>
                 </li>
               )
             })
+            
           }
         </ul>
       </div>
